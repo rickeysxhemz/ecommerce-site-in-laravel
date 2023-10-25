d811d880f0f017438c1871b407c76d55514ab807
rickeysxhemz <rickeysxhemz@gmail.com>
2023-10-26 00:32:48 +0500

    index page work

--- app/Services/HomeService.php
+++ app/Services/HomeService.php
@@ -8,9 +8,10 @@ class HomeService{
     public function index()
     {
         $featured_categories=Category::with('FeaturedCategory')->get();
+        $all_products=Product::all();
         $best_seller=Product::where('selling_count','>',10)->get();
-        // dd($best_seller);
-        return view('index',compact('featured_categories'));
+        $most_viewed=Product::where('item_view_count','>',10)->get();
+        return view('index',compact('featured_categories','all_products','best_seller','most_viewed'));
     }
     public function about()
     {
